8b491bb5d084897b2ed23822c59653716b840e24
David Daugela <daviddaugela@gmail.com>
2017-10-22 18:44:32 -0400

    Update websocket_server.py

--- rt-data-viz/websocket_server.py
+++ rt-data-viz/websocket_server.py
@@ -61,5 +61,5 @@ if __name__ == "__main__":
   #create new web app w/ websocket endpoint available at /websocket
   print ("Starting websocket server program. Awaiting client requests to open websocket ...")
   application = web.Application([(r'/websocket', WebSocketHandler)])
-  application.listen(8001)
+  application.listen(80)
   ioloop.IOLoop.instance().start()
